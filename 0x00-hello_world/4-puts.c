6b773f4aaa3f26c2ea693f820d623ecc24a988a6
iakev <kirimikmwongera@gmail.com>
2022-06-30 04:00:27 -0700

    Added some corrections to  4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
+	/* function to print a string using puts() */
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
