7a002d15cc277c9656bcc0aa093df8826e073449
iakev <kirimikmwongera@gmail.com>
2022-06-30 05:27:23 -0700

    wrote a c program that outputs to the standard output a string and a newline using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * program to print a specified string followed by a new line using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
