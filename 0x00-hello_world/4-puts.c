592fb1a31701d1b949fc408024198e7dfb976050
iakev <kirimikmwongera@gmail.com>
2022-06-30 04:06:22 -0700

    Made more corrections on 4-puts.c file to match the linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * function to print a string using puts()
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/* function to print a string using puts() */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
