fa6d3c0268da4709b1df51be520c7365397a0952
iakev <kirimikmwongera@gmail.com>
2022-07-01 05:05:56 -0700

    Added ac program that prints the alphabet using putchar() function only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print the alphabet using only putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string = 'a';
+
+	while (string < 'z')
+	{
+		putchar(string);
+		string++;
+	}
+	putchar('\n');
+	return (0);
+}
