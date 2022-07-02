8e51fafcf9a3f2b18bf6c5fac01f0c9aab7ed814
iakev <kirimikmwongera@gmail.com>
2022-07-02 01:38:51 -0700

    Added  the first advanced task 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print the alphabet using only putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base_16 = 0;
+	int base_16_2 = 0;
+
+	while (base_16 <= 9)
+	{
+		while (base_16_2 <= 9)
+		{
+			putchar(base_16 + '0');
+			putchar(base_16_2 + '0');
+			if (base_16 != 9 && base_16_2 != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			base_16_2++;
+		}
+		base_16_2 = 0;
+		base_16++;
+	}
+	putchar('\n');
+	return (0);
+}
