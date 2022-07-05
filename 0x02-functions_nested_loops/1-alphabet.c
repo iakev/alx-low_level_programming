6a0c5e0d6b34f150794913bd7fb617525aca5221
iakev <kirimikmwongera@gmail.com>
2022-07-05 10:44:25 +0000

    Added second correction for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point
- * function to print a string using _putschar() whos prototype is in main.h
+ * print_alphabet - fuction
+ * function to print alphabet and new line using _putschar()
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
