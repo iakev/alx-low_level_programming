9d536e4a470a0aab364e10a642c49682acb1fbd6
iakev <kirimikmwongera@gmail.com>
2022-07-05 21:19:09 +0000

    added source files for task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print upto 98 from n in either side
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			printf("%d, ",n);
+			n--;
+		}
+		else if (n < 98)
+		{
+			printf("%d, ",n);
+			n++;
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n",n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ void times_table(void);
 void inner_loop_if(int ent, int c, int f, int l);
 void inner_loop_else(int ent, int f, int c, int l);
 int add(int, int);
+void print_to_98(int n);
