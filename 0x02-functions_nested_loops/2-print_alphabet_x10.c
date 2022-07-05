053abc234483557cfe765ef51180046bc0996ff0
iakev <kirimikmwongera@gmail.com>
2022-07-05 10:56:10 +0000

    Added c program file for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - fuction to print alphabet and new line using
+ * _putschar() 10 times.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char *string = "abcdefghijklmnopqrstuvwxyz";
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j < 26)
+		{
+			_putchar(string[i]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
