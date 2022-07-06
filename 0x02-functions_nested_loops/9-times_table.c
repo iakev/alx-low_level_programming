a12190df7d4d7bbbc6d7ac1ca29d77992559197f
iakev <kirimikmwongera@gmail.com>
2022-07-06 05:18:24 +0000

    Made corrections on task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 void inner_loop_if(int ent, int c, int f, int l);
 void inner_loop_else(int ent, int c, int f, int l);
+
 /**
- * times_table - function to print the times_table upto 9
+ * times_table - displays multiplication table from 0 to 9
  *
- * Return: Nothing just prints.
+ * Return: Nothing only prints.
  */
 void times_table(void)
 {
-	int row =  0;
-	int numbers [10]  = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int row = 0;
 	int first = 0;
 	int last = 0;
 
@@ -24,13 +26,82 @@ void times_table(void)
 			entry = row * numbers[col];
 			next_entry = row * numbers[col + 1];
 			if (next_entry < 10)
-				inner_loop_if(entry, col, first,
-					      last);
+				inner_loop_if(entry, col, first, last);
 			else
 				inner_loop_else(entry, col, first, last);
 			col++;
 		}
 		_putchar('\n');
 		row++;
+	}
+}
+/**
+ * inner_loop_if - helper function to displays multiplication table from 0 to 9
+ * @ent: interger denoting the value of multiplication
+ * @c: interger representing columns of the table
+ * @f: first interger for dealing with double digits for _putchar()
+ * @l: last interger for dealing with doublw digits for _putchar()
+ * Return: Nothing only prints.
+ */
+void inner_loop_if(int ent, int c, int f, int l)
+{
+	{
+		if (ent < 10)
+		{
+			_putchar(ent + '0');
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			f = ent / 10;
+			l = ent % 10;
+			_putchar(f + '0');
+			_putchar(l + '0');
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
+/**
+ * inner_loop_else - helper function to displays multiplication table
+ * from 0 to 9
+ * @ent: interger denoting the value of multiplication
+ * @c: interger representing columns of the table
+ * @f: first interger for dealing with double digits for _putchar()
+ * @l: last interger for dealing with doublw digits for _putchar()
+ * Return: Nothing only prints.
+ */
+void inner_loop_else(int ent, int c, int f, int l)
+{
+	{
+		if (ent < 10)
+		{
+			_putchar(ent + '0');
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			f = ent / 10;
+			l = ent % 10;
+			_putchar(f + '0');
+			_putchar(l + '0');
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void inner_loop_if(int ent, int c, int f, int l);
-void inner_loop_else(int ent, int f, int c, int l);
+void inner_loop_else(int ent, int c, int f, int l);
 int add(int, int);
 void print_to_98(int n);
