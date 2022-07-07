29edcee9175b62166f27fb8c60a262a9aaf1a0e0
iakev <kirimikmwongera@gmail.com>
2022-07-07 12:11:29 +0000

    Added source files to complete task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print a dash line whose length depends on n using _putchar()
+ *
+ * @n: interger that determines number of dashes to be printed
+ * Return: nothing just print to the standard output stream
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
