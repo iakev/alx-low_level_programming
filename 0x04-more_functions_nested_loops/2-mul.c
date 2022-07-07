973aca3a2d47c64a4cc81265d207c47d288844d7
iakev <kirimikmwongera@gmail.com>
2022-07-07 10:21:26 +0000

    Added source fies for task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply two intergers
+ *
+ * @a: 1st interger to be multiplied
+ * @b: 2nd interger operand to be multiplied
+ * Return: interger result of a*b.
+ */
+int mul(int a, int b)
+{
+	int mult = 0;
+
+	mult = a * b;
+
+	return (mult);
+}
