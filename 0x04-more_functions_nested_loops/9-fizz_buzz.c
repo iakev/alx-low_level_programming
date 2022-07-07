b6a5392a64e70df8ac4c65f891211b518d42fa52
iakev <kirimikmwongera@gmail.com>
2022-07-07 14:37:52 +0000

    Added source files to complete task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - print numbers, fizz if number multilple of 3, buzz if multiple of 5
+ * and fizzbuzz of multiple of both 3 and 5
+ *
+ *
+ * Return: nothing just print to the standard output stream
+ */
+int main(void)
+{
+	int j;
+	char *div_5 = "Buzz";
+	char *div_3 = "Fizz";
+	char *div_35 = "FizzBuzz";
+
+	for (j = 1; j < 101; j++)
+	{
+		if (j % 3 == 0  && j % 5 == 0)
+			printf("%s ", div_35);
+		else if (j % 5 == 0)
+			printf("%s ", div_5);
+		else if (j % 3 == 0)
+			printf("%s ", div_3);
+		else
+			printf("%d ", j);
+	}
+	return (0);
+}
