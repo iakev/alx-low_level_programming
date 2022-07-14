b8ad6f349cc22bf4fd83811f84d3ad50c891fbd0
iakev <kirimikmwongera@gmail.com>
2022-07-14 08:47:14 +0000

    Added source files for task6 which capitalizes words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,15 +12,21 @@ char *cap_string(char *str)
 {
 	char *c_add;
 
-	c_add = s;
+	c_add = str;
+	if (*c_add >= 97 && *c_add <= 122)
+		*c_add -= 32;
 	while (*c_add != '\0')
 	{
-		if (*c_add >= 97 && *c_add <= 122)
+		if ((*c_add == 32 || *c_add == 9 || *c_add == 11 || *c_add == 10
+		     || *c_add == 44 || *c_add == 59 || *c_add == 46 || *c_add
+		     == 33 || *c_add == 63 || *c_add == 34 || *c_add == 40 ||
+		     *c_add == 41 || *c_add == 123 || *c_add == 125)
+		    && (*(c_add + 1) >= 97 && *(c_add + 1) <= 122))
 		{
-			*c_add -= 32;
+			*(c_add + 1) -= 32;
 		}
 		c_add++;
 	}
 
-	return (s);
+	return (str);
 }
