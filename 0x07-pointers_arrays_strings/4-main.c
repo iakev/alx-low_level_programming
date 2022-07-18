ca9dac8569d5ab6d4a96dfbceaeb772a61536d7b
iakev <kirimikmwongera@gmail.com>
2022-07-18 13:17:34 +0000

    Added source files for task4

---
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "geeksforgeeks";
+    char *f = "kite";
+    char *t;
+
+    t = _strpbrk(s, f);
+    if (t == NULL)
+    {
+	    printf("Null pointer alert\n");
+    }
+    else
+    {
+	    printf("%s\n", t);
+    }
+    return (0);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - function that locates first occurence in string 's' of any of the
+ * bytes in the string 'accept'
+ *
+ * @s: pointer to string 's' that is scanned
+ * @accept: string containing bytes that are searched for
+ *
+ * Return: a pointer to the byte in 's' that matches first occurence of bytes
+ * in 'accept' or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned long int i, j;
+	char *str = s;
+
+	for (i = 0; i <= strlen(s) ; i++)
+	{
+		for (j = 0; j < strlen(accept); j++)
+		{
+			if (s[i] == accept[j])
+			{
+				str = &s[i];
+				break;
+			}
+		}
+		if (*str == accept[j])
+			break;
+	}
+	if (s[i] == accept[j])
+	{
+		return (str);
+	}
+	else
+		return (NULL);
+}
