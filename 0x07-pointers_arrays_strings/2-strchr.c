4a63fa4a69403c78511b541db1eb4a5d1fffd270
iakev <kirimikmwongera@gmail.com>
2022-07-18 10:35:23 +0000

    Added source files for task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strchr - function that returns a pointer to the first occurence of 'c' in
+ * string 's' orf 'NULL' if character not found
+ *
+ * @s: pointer to string 's' that 'c' is searched for in
+ * @c: character whose first occurence is sought
+ *
+ * Return: a pointer to the first occurence of 'c' or to 'NULL'
+ */
+char *_strchr(char *s, char c)
+{
+	char *str;
+
+	str = s;
+	while (*str != '\0')
+	{
+		if (*str == c)
+			return (str);
+		str++;
+	}
+
+	return (NULL);
+}
