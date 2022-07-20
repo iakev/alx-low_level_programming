9bb921f80c447d5ca1104e98b288dbe51c39149e
iakev <kirimikmwongera@gmail.com>
2022-07-20 17:58:48 +0000

    Added source files for task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 int good_enough(int, int);
-int new_guess(int, int);
 int sqrt_recursive(int, int);
 /**
  * _sqrt_recursion - returns the square root of 'n'
@@ -14,8 +13,16 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else if (n == 1)
 		return (1);
-	return (sqrt_recursive(n/2, n));
+	return (sqrt_recursive(n / 2, n));
 }
+/**
+ * sqrt_recursive - helper function for square root
+ *
+ * @n: square root to be found of n
+ * @guess: guess of square root
+ * Return: int representing value of square root of 'n'
+ */
+
 int sqrt_recursive(int guess, int n)
 {
 	if (good_enough(guess, n))
@@ -28,9 +35,16 @@ int sqrt_recursive(int guess, int n)
 	}
 	else
 	{
-		return sqrt_recursive(guess - 1, n);
+		return (sqrt_recursive(guess - 1, n));
 	}
 }
+/**
+ * good_enough - checks if guess of square root is fine
+ *
+ * @n: square root to be found of n
+ * @guess: interger to be checked
+ * Return: int representing  a  bool value if true or not
+ */
 
 int good_enough(int guess, int n)
 {
