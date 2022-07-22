6614a19a552753bb4e46713021933f5953cf3809
iakev <kirimikmwongera@gmail.com>
2022-07-22 16:49:09 +0000

    made coirrections to align with the linter

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- *  - function that prints the name of the program
+ * main - function that prints the name of the program
+ * @argc: count of arguments numbers
+ * @argv: vector array of strings
  * Return: always zero sgnifyin
  */
-int main(__attribute__((unused)) int argc,char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
