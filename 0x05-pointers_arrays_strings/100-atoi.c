2198eb9ef4e44e2fc8da8e0fa519dd22a7c6c9b9
iakev <kirimikmwongera@gmail.com>
2022-07-25 13:52:13 +0000

    made corrections on advanced task100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,34 +10,5 @@
  */
 int _atoi(char *s)
 {
-	int count = 0;
-	char c;
 
-	c = s[count];
-	while (c != '\0');
-	{
-		if ('c' >= 48 && 'c' <= 57)
-		{
-			
-		}
-	}
-}
-
-int len_digit(char *s)
-{
-	int count = 0;
-	int i = 0;
-	char c;
-
-	c = s[0];
-	while (c != '\0')
-	{
-		if('c' >= 42 && 'c' <= 57)
-		{
-			count++;
-		}
-		else
-			break;
-	}
-	return count;
 }
