3a3a5b9c2ea2bbeaadf40ab4ed8cc4094f3e6e04
iakev <kirimikmwongera@gmail.com>
2022-07-26 13:22:12 +0000

    added source files for task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - allocates memory for a 2D array of intergers
+ *
+ * @width: interger repping the width of the 2D array
+ * @height: interger repping the height od the 2D array
+ * Return: a pointer to the newly allcoated memmory for the 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, *col, i;
+
+	col = malloc(sizeof(int) * width * height);
+	grid = malloc(sizeof(int *) * height);
+	if (col != NULL)
+	{
+		for (i = 0; i < width * height; i++)
+		{
+			col[i] = 0;
+		}
+	}
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			grid[i] = col;
+			col += width;
+		}
+	}
+
+	return (grid);
+}
