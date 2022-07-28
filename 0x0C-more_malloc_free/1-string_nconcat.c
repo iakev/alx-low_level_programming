71fc81c44bbf68cda2b43b96ce5c146edb1b5857
iakev <kirimikmwongera@gmail.com>
2022-07-28 14:02:35 +0000

    made corrections to task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+void preprocess(char *, char *, unsigned int *,
+		unsigned int *, unsigned int *);
 /**
  * string_nconcat - concatenates tow strings s1 and s2
  *
@@ -17,26 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = strdup("");
 		return (s);
 	}
-	else if (s1 == NULL)
-	{
-		size1 = 0;
-		size2 = strlen(s2);
-	}
-	else if (s2 == NULL)
-	{
-		size1 = strlen(s1);
-		size2 = 0;
-		n = 0;
-	}
-	else
-	{
-		size1 = strlen(s1);
-		size2 = strlen(s2);
-		if (n >= size2)
-		{
-			n = size2;
-		}
-	}
+	preprocess(s1, s2, &n, &size1, &size2);
 	s = malloc(sizeof(*s) * (size1 + n + 1));
 	if (s == NULL)
 	{
@@ -47,17 +30,53 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		if (i < size1)
 		{
 			s[i] = s1[i];
-			printf("s1[%d] is:%c\n s[%d] is:%c\n", i, s1[i],i,s[i]);
 		}
 		else
 		{
 			s[i] = s2[j];
-			printf("s2[%d] is:%c\n s[%d] is:%c\n", j,s2[j],i,s[i]);
 			j++;
 		}
 
 	}
-	s[i] = '\0';
-
+	s[size1 + n + 1] = '\0';
 	return (s);
+}
+/**
+ * preprocess - concatenates tow strings s1 and s2
+ *
+ * @s1: string added to a certain bit ro s2
+ * @s2: string to be added to s1 upto n
+ * @n: interger repersenting the extent that s2 is added to s1
+ * @size1: length of s1
+ * @size2: length of s2
+ * Return: nothing
+ */
+
+void preprocess(char *s1, char *s2, unsigned int *n,
+		unsigned int *size1, unsigned int *size2)
+{
+	if (s1 == NULL)
+	{
+		*size1 = 0;
+		*size2 = strlen(s2);
+		if (*n > *size2)
+		{
+			*n = *size2;
+		}
+	}
+	else if (s2 == NULL)
+	{
+		*size1 = strlen(s1);
+		*size2 = 0;
+		*n = 0;
+	}
+	else
+	{
+		*size1 = strlen(s1);
+		*size2 = strlen(s2);
+		if (*n >= *size2)
+		{
+			*n = *size2;
+		}
+	}
 }
