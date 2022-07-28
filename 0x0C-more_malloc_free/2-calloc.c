3c9a81e4aa9fc96181767407cd4f8222f1b44ba2
iakev <kirimikmwongera@gmail.com>
2022-07-28 14:45:04 +0000

    made corrections to task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,17 +8,23 @@
  */
 void *_calloc(unsigned int nmeb, unsigned int size)
 {
-	void *ptr, *arr;
+	unsigned int i;
+	void *ptr;
+	char *str;
 
 	if (size == 0 || nmeb == 0)
 	{
 		return (NULL);
 	}
-	ptr = malloc(nmeb * size);
-	if (ptr == NULL)
+	str = malloc(nmeb * size);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	arr = memset(ptr, 0, nmeb);
-	return (arr);
+	for (i = 0; i < nmeb; i++)
+	{
+		str[i] = 0;
+	}
+	ptr = (void*) str;
+	return (ptr);
 }
